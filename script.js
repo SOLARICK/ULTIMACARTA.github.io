53d19725bbff3a985cb0765a5ece926fc08b48d6
SOLARICK <joaquingaray945@gmail.com>
2026-02-14 00:36:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+function next1(){
+        window.location.href = "./page2.html";
+    }
+function next2(){
+        window.location.href = "./page3.html";
+    }
+function next3(){
+        window.location.href = "./page4.html";
+    }
+function next4(){
+        window.location.href = "./boton.html";
+    }
+function btnBien(){
+        window.location.href = "./sibien.html";
+    }    
+function siEstas(){
+        window.location.href = "./loco.html";
+    }   
+function btnMal(){
+        window.location.href = "./mal.html";
+    }
+function noCreo(){
+        window.location.href = "./noloco.html";
+    }     
+function siVerte(){
+        window.location.href = "./verte.html";
+    }
+function siInteres(){
+        window.location.href = "./interes.html";
+    }
+function noInteres(){
+        window.location.href = "./nointeres.html";
+    }
+function noVerte(){
+        window.location.href = "./noverte.html";
+    }
+function volver1(){
+        window.location.href = "./page2.html";
+    }   
+function volver2(){
+        window.location.href = "./page2.html";
+    }   
+function volver3(){
+        window.location.href = "./sibien.html";
+    }  
+function volver4(){
+        window.location.href = "./sibien.html";
+    }
+function volver5(){
+        window.location.href = "./loco.html";
+    }
+function volver6(){
+        window.location.href = "./page3.html";
+    }
+function volver7(){
+        window.location.href = "./page3.html";
+    }
+function volver8(){
+        window.location.href = "./noverte.html";
+    }
+function volver9(){
+        window.location.href = "./page4.html";
+    }
+function volver10(){
+        window.location.href = "./page4.html";
+    }
+
